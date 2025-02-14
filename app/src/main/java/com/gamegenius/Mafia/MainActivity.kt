e35ab7e707f2238eec164bccb0800be350fc7f0f
ColdBlood <programist228z@yandex.ru>
2025-02-14 13:14:40 +0300

    13.02.25

---
+++ app/src/main/java/com/gamegenius/Mafia/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.gamegenius.Mafia
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnKeyListener
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gamegenius.Mafia.ui.theme._10MafiaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        actionBar?.hide();
+
+        this.inzializate_start()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
+
+    fun inzializate_start(){
+        setContentView(R.layout.main)
+
+        var lb: TextView = this.findViewById(R.id.info_inizial_lb)
+        lb.setText("Городская мафия; Царский стол\n8 февраля 19:00 - 00:30")
+
+        var btn: ImageButton = this.findViewById(R.id.start_game_inizial_btn)
+        btn.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
+            when(motionEvent.action){
+                MotionEvent.ACTION_DOWN -> {
+                    println("down")
+                    true}
+                MotionEvent.ACTION_UP -> {
+                    println("up")
+                    true
+                }
+                else->{
+                    true
+                }
+            }})
+    }
+}
