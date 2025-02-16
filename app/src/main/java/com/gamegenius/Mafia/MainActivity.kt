448467bd64a5785888b603d845d1fc37c4668642
ColdBlood <programist228z@yandex.ru>
2025-02-16 14:26:06 +0300

    16.02.25

--- app/src/main/java/com/gamegenius/Mafia/MainActivity.kt
+++ app/src/main/java/com/gamegenius/Mafia/MainActivity.kt
@@ -20,11 +20,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.gamegenius.Mafia.ui.theme._10MafiaTheme
 
 class MainActivity : ComponentActivity() {
+    private var players: MutableList<Int> = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         actionBar?.hide();
-
+        setContentView(R.layout.lst_of_players_start)
         this.inzializate_start()
     }
 
@@ -45,18 +47,28 @@ class MainActivity : ComponentActivity() {
         lb.setText("Городская мафия; Царский стол\n8 февраля 19:00 - 00:30")
 
         var btn: ImageButton = this.findViewById(R.id.start_game_inizial_btn)
+
         btn.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
-            when(motionEvent.action){
+            when (motionEvent.actionMasked){
                 MotionEvent.ACTION_DOWN -> {
-                    println("down")
-                    true}
+                    // Здесь должна быть твоя анимация (начало)
+                    true
+                }
                 MotionEvent.ACTION_UP -> {
-                    println("up")
+                    // Здесь она должна закончиться
+                    if ((btn.width > motionEvent.getX() && motionEvent.getX() > 0) && (btn.height > motionEvent.getY() && motionEvent.getY() > 0)) {
+                        inizializate_lst_players()
+                    }
                     true
                 }
-                else->{
+                else -> {
                     true
                 }
-            }})
+            }
+        })
+    }
+
+    fun inizializate_lst_players(){
+        setContentView(R.layout.lst_of_players_start)
     }
 }
